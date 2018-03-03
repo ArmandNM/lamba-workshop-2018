7f27534a9974995ec76d4d9f49004e9085903d15
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2018-03-04 00:37:56 +0200

    Update trapping.rain.water.scala
    complete implementation.
    https://leetcode.com/submissions/detail/143279869/

--- week-2/trapping.rain.water.scala
+++ week-2/trapping.rain.water.scala
@@ -1,10 +1,41 @@
 case class State(leftStack: MaxStack, rightStack: MaxStack, rem: List[Int], runningSum: Int)
 
-val elevationList: List[Int] = List(0,1,0,2,1,0,1,3,2,1,2,1)
+// prepare the state members
+val elevationList: List[Int] = List(10,5,10)
 val reversedElevationList = elevationList.reverse
 val emptyMaxStack = MaxStack(List.empty[StackItem])
 val initialRightStack = reversedElevationList.foldLeft(emptyMaxStack){ case (accMaxStack, cValue) =>
   push(accMaxStack, cValue)
 }
 
-// [TODO] finish the problem
+val initialState = State(emptyMaxStack, initialRightStack, elevationList, 0)
+
+def transition(s: State): State = {
+  val restHeadOption = s.rem.headOption
+  restHeadOption.map { case restHead =>
+    val nextRest = s.rem.tail
+    val leftMax = max(s.leftStack).getOrElse(0)
+    val rightMax = max(s.rightStack).getOrElse(0)
+    
+    // Compute the water level at the current position
+    val waterLevel = scala.math.min(leftMax, rightMax) - restHead
+    val positiveWaterLevel = if (waterLevel > 0) {waterLevel} else {0}
+
+    // Construct the new State
+    val newRunningSum = s.runningSum + positiveWaterLevel
+    val newLeftStack = push(s.leftStack, restHead)
+    val (_, newRightStack) = pop(s.rightStack)
+    State(newLeftStack, newRightStack, nextRest, newRunningSum)
+  }.getOrElse {
+    s
+  }
+}
+
+// Get the result after iterating the whole list
+val trappedWaterVolume = {
+  Stream
+    .iterate(initialState)(transition)
+    .dropWhile { case State(_, _, rem, _) => rem.nonEmpty }
+    .head
+    .runningSum
+}
