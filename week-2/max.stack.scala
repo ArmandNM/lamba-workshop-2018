ba1025ab7bb53a217dd0f69530f8f6ff105b69a6
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2018-03-03 17:56:02 +0200

    Create max.stack.scala

---
+++ week-2/max.stack.scala
@@ -0,0 +1,29 @@
+case class StackItem(item: Int, thenMax: Int)
+case class MaxStack(items: List[StackItem])
+
+def push(s: MaxStack, value: Int): MaxStack = {
+  val topOption = s.items.headOption
+  topOption.map { case StackItem(item , thenMax) =>
+    val newMax = scala.math.max(item, thenMax)
+    MaxStack(StackItem(value, newMax) :: s.items)
+  }.getOrElse {
+    MaxStack(List(StackItem(value, Integer.MIN_VALUE)))
+  }
+}
+
+def pop(s: MaxStack): (Option[Int], MaxStack) = {
+  val topOption = s.items.headOption
+  topOption.map { case StackItem(item , _) =>
+    val restStack = MaxStack(s.items.tail)
+    (Some(item), MaxStack(s.items.tail))
+  }.getOrElse {
+    (None, s)
+  }
+}
+
+def max(s: MaxStack): Option[Int] = {
+  val topOption = s.items.headOption
+  topOption.map { case StackItem(item , thenMax) =>
+    scala.math.max(item, thenMax)
+  }
+}
