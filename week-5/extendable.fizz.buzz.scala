cba0a4f90826096e1ebe434ab36a60375afe6717
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2018-03-29 19:51:20 +0300

    fizz buzz complete ^^

--- week-5/extendable.fizz.buzz.scala
+++ week-5/extendable.fizz.buzz.scala
@@ -1,4 +1,15 @@
-def tokenToStream(token: String, index: Int): Stream[String] = {
+case class State(crtAnswer: List[String], listOfStreamedTokens: List[Stream[String]])
+
+def transition(s: State): State = {
+  val listofHeads = s.listOfStreamedTokens.map { case stream => stream.head }
+  val concatenatedHeads = listofHeads.reduce(_ + _)
+  val newAnswer = concatenatedHeads :: s.crtAnswer
+  val listOfStreamsWithoutHead = s.listOfStreamedTokens.map { case stream => stream.drop(1) }
+
+  State(newAnswer, listOfStreamsWithoutHead)
+}
+
+def tokenToStream(index: Int, token: String): Stream[String] = {
   // Create an initial list containing only empty strings
   val listEmptyString = List.fill(index - 1){""}
   // Add the token at the end of the list
@@ -9,14 +20,15 @@ def tokenToStream(token: String, index: Int): Stream[String] = {
   repeatingStream
 }
 
-def fizzBuzz(steps: Int, tokens: List[(String, Int)]): List[String] = {
-  val listOfStreamedTokens: List[Stream[String]] = tokens.map { case (token, index) => tokenToStream(token, index) }
-  val listOfZippedStreams = listOfStreamedTokens.transpose
-  lazy val answer = listOfZippedStreams.map {case list => list.reduce(_ + _)}
-  answer.take(steps).toList
-}
+def fizzBuzz(steps: Int, tokens: List[(Int, String)]): List[String] = {
+  val listOfStreamedTokens: List[Stream[String]] = tokens.map { case (index, token) => tokenToStream(index, token) }
 
+  val initialState = State(List[String](), listOfStreamedTokens)
 
-def tts(): Stream[String] = {
-
+  Stream
+    .iterate(initialState)(transition)
+    .drop(steps)
+    .head
+    .crtAnswer
+    .reverse
 }
